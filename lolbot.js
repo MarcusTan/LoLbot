ff2d2226ca948ef58aa645f8e56b4a8201b24bd5
Marcus Tan <marcus.m.tan@gmail.com>
2016-09-16 21:04:42 -0400

    further abstraction of methods

--- lolbot.js
+++ lolbot.js
@@ -1,96 +1,27 @@
 // libraries, folders, files
 const Discord = require('discord.js'),
-      fs = require('fs'),
-      configFile = 'config.json';
+      configFile = 'config.json',
       bot = new Discord.Client(),
       helpers = require('./lib/helpers'),
+      staticData = require('./lib/staticData'),
       adminCommands = require('./lib/adminCommands'),
+      cggCommands = require('./lib/championGGCommands'),
       lolCommands = require('./lib/lolCommands');
 
 // TODO
+// strict vs no strict?
+// redo docstrings for jsdocs
+// quickfind: support asian language + wait for page to load
+// check for resources to load + if html is there?
+// public vs private commands? i.e. what you see in command list is different
 // changing cb to promises
-// champion gg list helper
 // picture mmr
 // http://aram.lolalytics.com/champion/Akali/
-
-// static data
-let config;
-const regionsMmr = ['na', 'euw', 'eune'],
-      // Region -> PlatformID (https://developer.riotgames.com/docs/spectating-games)
-      regionsLive = {
-          'na' : { platform: 'NA1' },
-          'euw' : { platform: 'EUW1' },
-          'eune' : { platform: 'EUN1' },
-          'kr' : { platform: 'KR' },
-          'br' : { platform: 'BR1' },
-          'jp' : { platform: 'JP1' },
-          'oce' : { platform: 'OC1' },
-          'las' : { platform: 'LA2' },
-          'ru' : { platform: 'RU' },
-          'tr' : { platform: 'TR1' }
-      },
-      roles = ['top', 'middle', 'jungle', 'adc', 'support'];
-const commandsList = {
-        'commands': 'Lists all available commands',
-        'help': '\`\`\` help <command> \`\`\`\n' +
-                'Show the usage description of a given command',
-        'setname': '\`\`\` setname <name with spaces | namewithoutspaces> \`\`\`\n' +
-                'Set the name of the bot - the new name can contain spaces\n' +
-                'Choose carefully! You can only change the bot name twice per hour\n' +
-                '**Requires you to be a bot owner**',
-        'mmr': '\`\`\` mmr <summonername> [region] \n' +
-                ' Supported Regions: ' + regionsMmr + '\`\`\`\n' +
-            'Get\'s user mmr from whatismymmr.com\n' +
-            'If there are any spaces in the summoner name replace them with \'+\'\n' +
-            'If no region is given, lookup defaults to NA',
-        'bestrated': '\`\`\` bestrated [' + roles.join(' | ') + '] \`\`\`\n' +
-            'Retrieves the top 10 \"Best Rated\" champions according to champion.gg\n' +
-            'If a role is given, retrieves the best rated champions for the given role\n' +
-            'If no role is given, retrieves the best rated champions from all roles\n' +
-            '\**"Overall Performance takes more than win rate into account' + 
-            '- including play rate, ban rate, kda, gold, cs, damage and other role dependant stats.\"**',
-        'bestwinrate': '\`\`\` bestwinrate [' + roles.join(' | ') + '] \`\`\`\n' +
-            'Retrieves the top 10 champions with the highest winrate from champion.gg\n' +
-            'If a role is given, retrieves the highest winrate champions for the given role\n' +
-            'If no role is given, retrieves the highest winrate champions from all roles\n',
-        'live': '\`\`\` live <summonername> [region] \n' + 
-                ' Supported Regions: ' + regionsLive + '\`\`\`\n' +
-            'Get\'s live game info for a user from quickfind.kassad.in\n' +
-            'If there are any spaces in the summoner name replace them with \'+\'\n' +
-            'If no region is given, lookup defaults to NA\n' + 
-            'Does not work if a summoner is offline, or in a custom game'
-    };
-exports.regionsMmr = regionsMmr;
-exports.regionsLive = regionsLive;
-exports.roles = roles;
-exports.commandsList = commandsList;
-
-// read the config file and populate the config var
-const readConfig = (cb) => {
-    try {
-        console.log('Attempting to load `config.json` from ' + __dirname);
-        config = JSON.parse(fs.readFileSync(configFile)),
-        exports.config = config;
-        console.log('Config file successfully read');
-        cb();
-    } catch (err) {
-        console.log(err.message);
-        console.log('Please fix the issue and restart')
-    }   
-}
-
-// Error check config options
-const populateOptions = () => {
-    if (!config.commandPrefix) {
-        console.log('Please set a valid command prefix');
-        process.exit(1);
-    }
-}
+// builds for SR and ARAM
 
 // Read the config, log the bot in, populate options
-readConfig(() => {
-    bot.login(config.DiscordBotToken);
-    populateOptions();
+staticData.readConfig(configFile, () => {
+    bot.login(staticData.config.DiscordBotToken);
 });
 
 bot.on('ready', () => {
@@ -104,49 +35,58 @@ bot.on('error', (error) => {
 // bot commands
 bot.on('message', msg => {
     /* ---------- Admin Commands ---------- */
-    //commands
-    if (msg.content.startsWith(config.commandPrefix + 'commands')) {
+    // commands
+    if (msg.content.startsWith(staticData.config.commandPrefix + 'commands')) {
         helpers.logCommand(msg);
         msg.channel.sendMessage(adminCommands.getCommands());
     }
     // help
-    if (msg.content.startsWith(config.commandPrefix + 'h')) {
+    if (msg.content.startsWith(staticData.config.commandPrefix + 'h')) {
         helpers.logCommand(msg);
         msg.channel.sendMessage(adminCommands.getHelp(msg.content.split(' ')));
     }
     // set the name of bot
-    if (msg.content.startsWith(config.commandPrefix + 'setname')) {
+    if (msg.content.startsWith(staticData.config.commandPrefix + 'setname')) {
         helpers.logCommand(msg);
         adminCommands.setName(msg.content.split(' '), msg, bot.user);
     }
 
+    /* ---------- ChampionGG Commands ---------- */
+    // championgg - mostBanned
+    if (msg.content.startsWith(staticData.config.commandPrefix + 'mostbanned')) {
+        helpers.logCommand(msg);
+        cggCommands.getMostBanned(msg.content.split(' '), (retMsg) => {
+            msg.channel.sendMessage(retMsg);
+        });
+    }
+
+    // championgg - bestRated
+    if (msg.content.startsWith(staticData.config.commandPrefix + 'bestrated')) {
+        helpers.logCommand(msg);
+        cggCommands.getBestRated(msg.content.split(' '), (retMsg) => {
+            msg.channel.sendMessage(retMsg);
+        });
+    }
+
+    // championgg - bestWinrate
+    if (msg.content.startsWith(staticData.config.commandPrefix + 'bestwinrate')) {
+        helpers.logCommand(msg);
+        cggCommands.getBestWinrate(msg.content.split(' '), (retMsg) => {
+            msg.channel.sendMessage(retMsg);
+        });
+    }
+
     /* ---------- LoL Commands ---------- */
     // whatismymmr
-    if (msg.content.startsWith(config.commandPrefix + 'mmr')) {
+    if (msg.content.startsWith(staticData.config.commandPrefix + 'mmr')) {
         helpers.logCommand(msg);
         lolCommands.getMmr(msg.content.split(' '), (retMsg) => {
             msg.channel.sendMessage(retMsg);
         });
     }
 
-    // championgg - bestRated
-    if (msg.content.startsWith(config.commandPrefix + 'bestrated')) {
-        helpers.logCommand(msg);
-        lolCommands.getBestRated(msg.content.split(' '), (retMsg) => {
-            msg.channel.sendMessage(retMsg);
-        });
-    }
-
-    // championgg - winrate
-    if (msg.content.startsWith(config.commandPrefix + 'bestwinrate')) {
-        helpers.logCommand(msg);
-        lolCommands.getBestWinrate(msg.content.split(' '), (retMsg) => {
-            msg.channel.sendMessage(retMsg);
-        });
-    }
-
-    // Screencap quickfind.kassad.in for live games
-    if (msg.content.startsWith(config.commandPrefix + 'live')) {
+    // quickfind.kassad.in
+    if (msg.content.startsWith(staticData.config.commandPrefix + 'live')) {
         helpers.logCommand(msg);
         lolCommands.getLive(msg.content.split(' '), (liveGame) => {
             if (liveGame.picPath) {
@@ -158,3 +98,4 @@ bot.on('message', msg => {
     }      
 
 });
+
